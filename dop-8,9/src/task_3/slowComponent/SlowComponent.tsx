cba543360b33bc43f47e3e643ea8375442387967
yneskladovae <yneskladovae27@gmail.com>
2023-04-23 22:03:45 +0300

    add dop - 8,9

---
+++ dop-8,9/src/task_3/slowComponent/SlowComponent.tsx
@@ -0,0 +1,13 @@
+import {memo} from "react";
+
+export const SlowComponent = memo(() => {
+  console.log('SlowComponent re-render...');
+
+  let now = performance.now();
+
+  while (performance.now() - now < 1000) {
+    // Artificial delay -- do nothing for 100ms
+  }
+
+  return <p>I am a very slow component tree.</p>;
+});
